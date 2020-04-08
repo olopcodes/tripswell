3a1c86599345db0330dbd1b6f5410d79f3f28f63
= <=>
2020-04-07 21:25:22 -0400

    made modal responsive and fixed the showModal js file to display when btn is clicked again

--- app/assets/scripts/modules/ShowModal.js
+++ app/assets/scripts/modules/ShowModal.js
@@ -1,47 +1,51 @@
 class ShowModal {
     constructor() {
-        this.btns = document.querySelectorAll('.nav__sign a');
         this.modal = document.querySelector('.modal');
-        this.signup = document.querySelector('.modal__signup');
-        this.login = document.querySelector('.modal__login');
+        this.loginBtn = document.querySelector('.nav__login');
+        this.signupBtn = document.querySelector('.nav__signup');
+        this.signupModal = document.querySelector('.modal__signup');
+        this.loginModal = document.querySelector('.modal__login');
         this.closeModal = document.querySelector('.modal__close');
         this.events();
     }
-
+    
     events () {
-        this.btns.forEach((btn) => {
-            btn.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.display(btn);
-            })
-        })
+        this.loginBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.showLogin();
+        });
+
+        this.signupBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.showSignup();
+        });
 
         this.closeModal.addEventListener('click', ()=> {
             this.modalClose();
         });
     }
 
-    display(btn) {
-        console.log('clicked')
-        const value = btn.innerText;
+    showLogin() {
+        console.log('clicked login')
         this.modal.style.display = 'block';
+        this.loginModal.classList.toggle('display');
+        
+    }
 
-        if (value.toLowerCase() === 'login') {
-            // this.login.style.transform = 'scale(1)';
-            // this.signup.style.display = 'none';
-            this.login.classList.toggle('display');
-            this.signup.classList.remove('display');
-        } else {
-            // this.signup.style.transform = 'scale(1)';
-            // this.login.style.display = 'none';
-            this.signup.classList.toggle('display');
-            this.login.classList.remove('display');
-        }
-
+    showSignup(){
+        console.log('clicked signup')
+        this.modal.style.display = 'block';
+        this.loginModal.classList.toggle('display');
     }
 
     modalClose () {
         this.modal.style.display = 'none';
+        // check which modal has the display class and remove
+        if(this.loginModal.classList.contains('display')) {
+            this.loginModal.classList.remove('display');
+        } else {
+            this.signupModal.classList.remove('display');
+        }
     }
 }
 
